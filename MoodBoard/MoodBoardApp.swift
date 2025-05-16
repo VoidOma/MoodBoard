0735c76bf47e1aac1c66e41deac2ea1de341b692
Milan <MilanMatejka38@gmail.com>
2025-05-16 14:28:08 +0200

    Initial Commit

---
+++ MoodBoard/MoodBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBoardApp.swift
+//  MoodBoard
+//
+//  Created by Milan Matejka on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
